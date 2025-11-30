870dd0c60e4bb4f255d9aefc8caeb20536edf8dd
Md Nehal Ahmad <nehal.ahmad@paynways.com>
2025-11-30 11:51:57 +0530

    first commit

---
+++ src/main/java/com/webapp/Javawebapp2Application.java
@@ -0,0 +1,13 @@
+package com.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javawebapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javawebapp2Application.class, args);
+	}
+
+}
